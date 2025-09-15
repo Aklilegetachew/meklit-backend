142af827b9b23ef8b25b5fcf269d10b3e02706c5
Aklilegetachew <aklilegetachew@gmail.com>
2025-09-15 17:47:27 +0300

    features and api included

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,12 @@
 import admin from "firebase-admin"
 
+//for dev
+// const serviceAccount = require("../../firebaseconfig.json")
 
-const serviceAccount = require("../../firebaseconfig.json")
+const serviceAccountPath = "/etc/secrets/firebaseconfig.json"
+
+// Parse the JSON file
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"))
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
